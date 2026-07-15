28304da2d91cd50829233145d4dbf41aabc148cb
Alan Ribeiro <166681521+Alanribc@users.noreply.github.com>
2026-07-15 23:41:38 +0000

    integração back + front tarefas

--- frontend/taskflow/src/app/services/api.js
+++ frontend/taskflow/src/app/services/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+console.log(API_URL);
 
 export async function getTarefas () {
     const resp = await fetch(`${API_URL}/tarefas`);
@@ -8,4 +10,32 @@ export async function getTarefas () {
     }
 
     return resp.json();
+}
+
+export async function criarTarefa(tarefa) {
+    const resp = await fetch(`${API_URL}/tarefas`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tarefa),
+    });
+
+    return resp.json();
+}
+
+export async function atualizarTarefa(id, tarefa) {
+    await fetch(`${API_URL}/tarefas/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tarefa),
+    });
+}
+
+export async function deletarTarefa(id) {
+    await fetch(`${API_URL}/tarefas/${id}`, {
+        method: "DELETE",
+    });
 }
--- frontend/taskflow/src/app/tarefas/page.js
+++ frontend/taskflow/src/app/tarefas/page.js
@@ -7,14 +7,22 @@ export default async function PageTarefas(){
     return (
         <section>
             <article>
-                <h1>Minhas tarefas</h1>
-                {tarefas.map((tarefa) => (
-                    <div key = {tarefa.id}>
-                        <h2>{tarefa.titulo}</h2>
-                        <p>{tarefa.dataHora}</p>
-                    </div>
-                ))}
+                <h1 className="minhasTarefas">Minhas tarefas</h1>
+
+                {tarefas.length === 0 ? (
+                    <p>Nenhuma tarefa cadastrada.</p>
+                ): (
+                    tarefas.map((tarefa) => (
+                        <article className="cardTarefa" key = {tarefa.id}>
+                            <h2>{tarefa.titulo}</h2>
+                            <p>{tarefa.descricao}</p>
+                            <span>{tarefa.status}</span>
+                            <span>{tarefa.prioridade}</span>
+                        </article>
+                    ))
+                )}
+
             </article>
         </section>
-    )
+    );
 }

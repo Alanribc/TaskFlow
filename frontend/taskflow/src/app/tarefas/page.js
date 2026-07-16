a91b168fd7a17d67cc3cb2a2d487917a7caeea97
Alan Ribeiro <166681521+Alanribc@users.noreply.github.com>
2026-07-16 04:10:17 +0000

    ajuste css tarefas

--- frontend/taskflow/src/app/tarefas/page.js
+++ frontend/taskflow/src/app/tarefas/page.js
@@ -7,21 +7,25 @@ export default async function PageTarefas(){
     return (
         <section>
             <article>
-                <h1 className="minhasTarefas">Minhas tarefas</h1>
-
-                {tarefas.length === 0 ? (
-                    <p>Nenhuma tarefa cadastrada.</p>
-                ): (
-                    tarefas.map((tarefa) => (
-                        <article className="cardTarefa" key = {tarefa.id}>
-                            <h2>{tarefa.titulo}</h2>
-                            <p>{tarefa.descricao}</p>
-                            <span>{tarefa.status}</span>
-                            <span>{tarefa.prioridade}</span>
-                        </article>
-                    ))
-                )}
-
+                <div>
+                    <h1 className="minhasTarefas">Minhas tarefas</h1>
+                </div>
+                <article className="repositorioTarefas">
+                    {tarefas.length === 0 ? (
+                        <p>Nenhuma tarefa cadastrada.</p>
+                    ): (
+                        tarefas.map((tarefa) => (
+                            <article className="cardTarefa" key = {tarefa.id}>
+                                <section className="tituloTarefa">
+                                    <h2>{tarefa.titulo}</h2>
+                                </section>
+                                <p>{tarefa.descricao}</p>
+                                <span>{tarefa.status}</span>
+                                <span>{tarefa.prioridade}</span>
+                            </article>
+                        ))
+                    )}
+                </article>
             </article>
         </section>
     );
